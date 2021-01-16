957201b7f50da847d2848cd9a5cb7d1370b2e147
Konischev Dmitriy <72737936+Konischevd@users.noreply.github.com>
2021-01-16 23:12:20 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        ApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorldBean1 =
+                (HelloWorld) context.getBean("helloworld");
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) context.getBean("helloworld");
+
+        Cat catBean1 = (Cat) context.getBean("cat");
+        Cat catBean2 = (Cat) context.getBean("cat");
+
+        System.out.println(helloWorldBean1 == helloWorldBean2);
+        System.out.println(catBean1 == catBean2);
+
     }
 }
